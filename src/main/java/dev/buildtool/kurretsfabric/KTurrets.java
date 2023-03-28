f998c04caeaca2ff7c9b2cc19649e3669c3dcaf0
alexiy <alexiy.ov@gmail.com>
2023-03-28 11:06:10 +0300

    Initial setup

---
+++ src/main/java/dev/buildtool/kurretsfabric/KTurrets.java
@@ -0,0 +1,10 @@
+package dev.buildtool.kurretsfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KTurrets implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/buildtool/kurretsfabric/client/KTurretsClient.java
@@ -0,0 +1,10 @@
+package dev.buildtool.kurretsfabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KTurretsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
